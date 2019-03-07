c9008faaa8e7025d94effa1f4e745282ff4bb903
sandhugagandeep9810@gmail.com <sandhugagandeep9810@gmail.com>
2019-03-07 15:07:51 +1300

    Add project files.

---
+++ hashtable/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace hashtable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnclear = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.lbxCount = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(375, 140);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 0;
+            this.btnclear.Text = "clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(350, 54);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(317, 99);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 23);
+            this.btnCount.TabIndex = 2;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // lbxCount
+            // 
+            this.lbxCount.FormattingEnabled = true;
+            this.lbxCount.Location = new System.Drawing.Point(588, 63);
+            this.lbxCount.Name = "lbxCount";
+            this.lbxCount.Size = new System.Drawing.Size(120, 173);
+            this.lbxCount.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 304);
+            this.Controls.Add(this.lbxCount);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnclear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.ListBox lbxCount;
+    }
+}
+
